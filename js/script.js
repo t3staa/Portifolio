0acb9fe3166c8364bedb5cf0241e49c11e1022a6
Leo-dev04 <desenvolvimento04@conecta.ind.br>
2025-01-29 17:06:36 -0300

    feat: adding carousel

--- js/script.js
+++ js/script.js
@@ -20,38 +20,40 @@ try {
 
   let repositoriesInfo = [];
   repositories.forEach(repository => {
-    repositoriesInfo.push({"name": repository.name, "link": repository.html_url, "desc": repository.description});
+    repositoriesInfo.push({"name": repository.name, "link": repository.html_url, "desc": repository.description})
   });
   console.log(repositoriesInfo);
 
-  const cardsRepositories = document.querySelector('#project');
+  const cardsRepositories = document.querySelector('#carousel');
 
   repositoriesInfo.forEach(repository => {
-    cardsRepositories.innerHTML += `<div class="projects-items">
+    cardsRepositories.innerHTML += `<li id="card">
                                       <img src="img/githubimg.png" alt="Projeto ${repository.name}">
                                       <h3><a href="${repository.link}" target="_blank">${repository.name}</a></h3>
                                       <p>${repository.desc}</p>
                                       <a href="${repository.link}" target="_blank" id="arrowBtn"><i class="fa-solid fa-arrow-right"></i></a>
-                                    </div>`
+                                    </li>`
   });
 
   const projects = document.querySelectorAll(".projects-items p");
-  const LIMIT = 60;
+  const LIMIT = 50;
 
   for (let p of projects){
     const aboveLimit = p.innerText.length > LIMIT;
     const dotsOrEmpty = aboveLimit ? '...' : '';
     p.innerText = p.innerText.substring(0, LIMIT) + dotsOrEmpty;
   }
-
-  // repositoriesInfo.forEach(repository => {
-  //   console.log(`Nome do repositorio: ${repository.name} Link: ${repository.link}`);
-  // })
-  
 } catch (error) {
   console.log(error);
   throw new Error(error);
 }
 
+const carousel = document.querySelector("#carousel");
+const arrowBtns = document.querySelectorAll(".button-carousel");
+const fisrtCardWidth = carousel.querySelector("#card").offsetWidth;
 
-
+arrowBtns.forEach(btn => {
+  btn.addEventListener("click", () => {
+    carousel.scrollLeft += btn.id === "left" ? -fisrtCardWidth : fisrtCardWidth;
+  })
+});
